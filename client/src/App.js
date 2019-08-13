199c39c4ca3846647d378118de7672539a49790d
Alexander Rudakov <alexander.rudakov1992@gmail.com>
2019-08-13 15:05:25 -0400

    routes partly done, waiting for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path='/' component/>
+            <Route path='/warehouses' component/>
+            <Route path='/warehouses/:id' component/>
+            <Route path='/inventory' component/>
+            <Route path='/inventory/:id' component/>
+          </Switch>
+        </Router>
+      </div>
+    )}
 }
 
 export default App;
