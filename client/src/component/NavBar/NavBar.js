a64d2e1f34ece35fe61722c8b6376d13cf54ee6c
yuri gwon <ilchanggwon@gmail.com>
2019-08-13 16:53:20 -0400

    Mobile completed

--- client/src/component/NavBar/NavBar.js
+++ client/src/component/NavBar/NavBar.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import logoInstock from "../../assets/Logo/Logo-instock.svg"
+import "./NavBar.scss";
 class NavBar extends React.Component {
     render () {
         return (
-            <p>demo</p>
+            <>
+            <div className="navbar-container">
+                <div className="navbar-container__img-wrapper">
+                    <img className="navbar-container__img-wrapper__img"src={logoInstock} alt="logo instock"></img>
+                </div>
+                <div className="navbar-container__text-wrapper">
+                    <div className="navbar-container__text-wrapper__text">Inventory</div>
+                    <div className="navbar-container__text-wrapper__text">Locations</div>
+                </div>
+            </div>
+            </>
         )
     }
 }
+
+export default NavBar;
