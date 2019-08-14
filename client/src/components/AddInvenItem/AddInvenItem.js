6dc3cb6a98c008ea4be24d2c0eea9ad1b99430e6
yuri gwon <ilchanggwon@gmail.com>
2019-08-14 14:23:05 -0400

    add new inventory item-vanvleet-17 component begins

---
+++ client/src/components/AddInvenItem/AddInvenItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./AddInvenItem.scss";
+
+class AddInvenItem extends React.Component {
+    render() {
+        return (
+            <>
+            <div className="title">Create New</div>
+            <div className="input-boxes">
+                <div></div>
+            </div>
+            </>
+        )
+    }
+}
+
+export default AddInvenItem;
