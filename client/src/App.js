7f6d540ce8e72947ef18adc2ef14b501da74878d
yuri gwon <ilchanggwon@gmail.com>
2019-08-14 15:03:26 -0400

    toggle button added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Inventory from'./components/Inventory/Inventory.js';
 import MainPage from './components/MainPage/MainPage.js';
 import Locations from './components/Locations/Locations.js';
+import AddInvenItem from './components/AddInvenItem/AddInvenItem.js';
 
 class App extends React.Component {
   render(){
@@ -13,7 +14,8 @@ class App extends React.Component {
           <Switch>
             <Route path='/inventory' component={Inventory} />
             <Route path='/inventory/:id' component/>
-            <Route exact path='/' component={MainPage} />
+          {/* Added AddInvenItem Instead of MainPage for Demo Display */}
+            <Route exact path='/' component={AddInvenItem} />
             <Route path='/warehouses' component= {Locations}/>
             {/* <Route path='/warehouses/:id' component/>
             <Route path='/inventory' component/>
--- client/src/components/AddInvenItem/AddInvenItem.js
+++ client/src/components/AddInvenItem/AddInvenItem.js
@@ -1,16 +1,47 @@
 import React from "react";
 import "./AddInvenItem.scss";
-
+import Switch from "react-switch";
 class AddInvenItem extends React.Component {
+state = { checked: false };
+ 
+handleChange(checked) {
+    this.setState({ checked });
+}
     render() {
         return (
             <>
             <div className="title">Create New</div>
             <div className="input-boxes">
-                <div></div>
+                <div>
+                    <div>PRODUCT</div>
+                    <input type="text" placeholder="Item Name" />
+                </div>
+                <div>
+                    <div>LAST ORDERED</div>
+                    <input type="text" placeholder="yyyy-mm-dd" />
+                </div>
+                <div>
+                    <div>CITY</div>
+                    <input type="text" placeholder="City" />
+                </div>
+                <div>
+                    <div>COUNTRY</div>
+                    <input type="text" placeholder="Canada" />
+                </div>
+                <div>
+                    <div>QUANTITY</div>
+                    <input type="text" placeholder="0" />
+                </div>
+                <div>
+                    <div>STATUS</div>
+                    <label>
+                    <div>In Stock</div>
+                    <Switch onChange={this.handle} checked={this.state.checked}/>
+                    </label>
+                </div>
             </div>
             </>
-        )
+        );
     }
 }
 
