a4adb7e00225214e7b4c6d451a35058fc407bce2
Omar Khan <o.khandxb@gmail.com>
2019-08-14 14:43:42 -0400

    added get request handling for ids

--- server/routes/warehouseRoutes.js
+++ server/routes/warehouseRoutes.js
@@ -8,4 +8,11 @@ router.get('/', (req, res) => {
   res.json(warehouseData);
 })
 
+router.get('/warehouse/:id', (req, res) => {
+  console.log(req.params.id);
+  var warehouse = warehouseData.filter(item => item.id === req.params.id)
+  res.json(warehouse);
+})
+
+
 module.exports = router;
