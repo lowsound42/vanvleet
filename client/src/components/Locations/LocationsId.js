04082be69701b6af64942fd52140e7a7b1e4517b
Armugan Ashraf <armugana1@gmail.com>
2019-08-15 20:29:40 -0400

    made some styling changes to warehouse/:id page

--- client/src/components/Locations/LocationsId.js
+++ client/src/components/Locations/LocationsId.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import NavBar from '../NavBar/NavBar';
+import arrowback from './Icons/SVG/Icon-back-arrow.svg';
+import {Link} from 'react-router-dom';
+import './LocationsId.scss';
 
 class LocationId extends React.Component{
 
@@ -10,16 +13,6 @@ class LocationId extends React.Component{
     contact: {}
   }
 
-// getData = () => {
-//   axios.get(`http://localhost:8080/warehouses/`)
-//   .then(response => {
-//     console.log(response.data)
-//     var product = response.data;
-//     this.setState({
-//       items: product
-//     });
-//   })
-// }
   componentDidMount(){
   axios.get(`http://localhost:8080/warehouse/${this.props.match.params.id}`)
   .then(response => {
@@ -41,20 +34,23 @@ class LocationId extends React.Component{
     return(
       <>
       <NavBar />
-      <div>
-      <div className='item'>
-        <div className='item__details'>
-          <h4 className='item__details--header'>{this.state.items.name}</h4>
-          <h4 className='item__details--header'>ADDRESS</h4>
-          <p className='item__details--info'>{this.state.address.street} {this.state.address.suiteNum}</p>
-          <p>{this.state.address.city},{this.state.address.province}</p>
-          <p>{this.state.address.postal}  CA</p>
-          <h4 className='item__details--header'>CONTACT</h4>
-          <p className='item__details--info'>{this.state.contact.name}  {this.state.contact.title}</p>
-          <p>{this.state.contact.phone}  {this.state.contact.email}</p>
+      <div className='location'>
+        <div className='location__details'>
+          <h1 className='location__details--header1'>
+          <Link to="/warehouses"><img src={arrowback}/></Link> {this.state.items.name}</h1>
+          <h4 className='location__details--header'>ADDRESS</h4>
+          <p className='location__details--info'>{this.state.address.street}</p>
+          <p className='location__details--info'>{this.state.address.suiteNum}</p>
+          <p className='location__details--info'>{this.state.address.city},{this.state.address.province}</p>
+          <p className='location__details--info'>{this.state.address.postal}  CA</p>
+          <h4 className='location__details--header'>CONTACT</h4>
+          <div>
+            <p className='location__details--info'>{this.state.contact.name} </p>
+            <p className='location__details--info'>{this.state.contact.title}</p>
+          </div>
+          <p className='location__details--info'>+ {this.state.contact.phone}</p> 
+          <p className='location__details--info'> {this.state.contact.email}</p>
         </div>
-        <button className='item__button'>EDIT</button>
-      </div>
     </div>
       </>
     )
